04548990a16bdd8be38f5a3591e05b0249941fb9
Benko Balog <benkobalog@gmail.com>
2018-02-09 11:11:20 +0100

    Remove implementation

--- src/main/scala/SettingsParser.scala
+++ src/main/scala/SettingsParser.scala
@@ -2,10 +2,6 @@ import scala.util.Try
 
 object SettingsParser {
 
-  object intFromString {
-    def unapply(arg: String): Option[Int] = Try(arg.toInt).toOption
-  }
-
   /** Produce a correct Setting object form command line arguments
     * Check out Setting.helpText for the parameters to be handled
     *  */
