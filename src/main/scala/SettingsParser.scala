2079be575eb006cfe5fd2c2b36684fd646ae10b1
Benko Balog <benkobalog@gmail.com>
2018-02-09 11:12:58 +0100

    Solutions

--- src/main/scala/SettingsParser.scala
+++ src/main/scala/SettingsParser.scala
@@ -6,6 +6,9 @@ object SettingsParser {
     def unapply(arg: String): Option[Int] = Try(arg.toInt).toOption
   }
 
+  /** Produce a correct Setting object form command line arguments
+    * Check out Setting.helpText for the parameters to be handled
+    *  */
   def apply(args: List[String]): Setting = {
     args match {
       case ("-h" | "--help") :: tail => apply(tail).copy(isHelp = true)
