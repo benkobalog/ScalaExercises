77aaf8e706edb2a2758052ff03006ad8c58e324d
benko_balog <benko.balog@gmail.com>
2018-02-23 10:06:15 +0100

    fix Main

--- scala-monads/src/main/scala/Main.scala
+++ scala-monads/src/main/scala/Main.scala
@@ -3,8 +3,8 @@ object Main extends App {
   def hearFirstNumber(): Option[Int] = Some(4)
   def hearPredicate(): Option[String] = Some("than 5")
 
-  val result = CompareMachine.isCorrect(hearFirstNumber(), hearPredicate())
+  val result = CompareMachineSolution.isCorrect(hearFirstNumber(), hearPredicate())
 
-  println(CompareMachine.showResult(result))
+  println(CompareMachineSolution.showResult(result))
 
 }
