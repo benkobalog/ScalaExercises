c139c75cb924702e99a9413339ba416443c7d161
benko_balog <benko.balog@gmail.com>
2018-03-09 11:16:16 +0100

    changes

--- implicits/build.sbt
+++ implicits/build.sbt
@@ -6,9 +6,16 @@ scalaVersion := "2.12.3"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.0.4" % "test"
 
-val http4sVersion = "0.18.0"
 val LogbackVersion = "1.2.3"
 
 libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-classic" % LogbackVersion
 )
+
+val circeVersion = "0.9.1"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- implicits/src/main/scala/Main.scala
+++ implicits/src/main/scala/Main.scala
@@ -1,14 +1,21 @@
+import java.util.Date
+
+import io.circe.{Encoder, Json, JsonObject}
 import json.JsWriterTypes._
 import testmodels._
 
 object Main extends App {
 
+//// Task 1
+
 //  import PipeOps._
 //  val res = "Mqtpmgmxw$evi$gssp%" |> (_.map(x => (x - 4).toChar))
 //  println(res)
 
+//// Task 2
+
 //  // Here are the solutions for Anonymous and User
-//  //import testmodels.VisitorTypeClassInstances._
+//  //  import testmodels.VisitorTypeClassInstances._
 //
 //  println("".toJson.stringify)
 //  println(123.toJson.stringify)
@@ -18,6 +25,22 @@ object Main extends App {
 //  val b = User("2", "test@test.com").toJson
 //  println(b.stringify)
 
+//// Task 3
 
+//  import io.circe.generic.auto._, io.circe.syntax._
+//
+//  case class Person(name: String)
+//  case class Greeting(salutation: String, person: Person, exclamationMarks: Int)
+//
+//  val res = Greeting("Hey", Person("Chris"), 3).asJson
+//  println(res.toString())
 
+//// Task 4
+
+//  import testmodels._
+//  val res2 = Anonymous("1").asJson
+//  println(res2.toString())
+//
+//  val res3 = User("2", "test@test.com").asJson
+//  println(res3.toString)
 }
