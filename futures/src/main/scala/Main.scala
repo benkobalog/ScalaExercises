fde80590750441df3323fcc4c6a6866c8ae640ac
benkbalog <benko.balog@gmail.com>
2018-03-22 23:00:28 +0100

    refactor

--- futures/src/main/scala/Main.scala
+++ futures/src/main/scala/Main.scala
@@ -12,17 +12,31 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    val url = "http://portfotolio.net/medvekoma?page=1"
+    def generateUrl = {
+      var id = 0
+      def gen() = {
+        id += 1
+        "http://portfotolio.net/medvekoma?page=" + id
+      }
+      gen _
+    }
 
-    def getAllImgs(url: String) = {}
 
-    val downloadAllJpgs = for {
-      htmlContent <- downloadPage(url)
-      jpgUrls <- extractJpgUrls(htmlContent)
-      _ <- downloadImages(jpgUrls)
-    } yield jpgUrls
+    def getAllImgs(genUrl:() => String, urls: Set[String]): Future[Set[String]] = {
+      val url = genUrl()
+      debug(s"Page: $url")
+      for {
+        htmlContent <- downloadPage(url)
+        jpgUrls <- extractJpgUrls(htmlContent)
+        res <- if (jpgUrls.nonEmpty) getAllImgs(genUrl, urls ++ jpgUrls) else Future.successful(urls)
+      } yield res
+    }
 
-    Await.result(downloadAllJpgs, 10.seconds)
+    val allJpgUrls = getAllImgs(generateUrl, Set())
+//    val downloadedImages = allJpgUrls.flatMap(downloadImages)
+
+    val result = Await.result(allJpgUrls, 10.seconds)
+    println(result)
 
     Client.backend.close()
   }
--- futures/src/main/scala/scraper/Links.scala
+++ futures/src/main/scala/scraper/Links.scala
@@ -5,7 +5,9 @@ import scala.concurrent.Future
 object Links {
   def extractJpgUrls(html: String): Future[Set[URL]] = {
     val jpgUrlR = """https?:\/\/.*\.jpg""".r
-    Future.successful(jpgUrlR.findAllIn(html).toSet)
+    val urls = jpgUrlR.findAllIn(html).toSet
+    println(s"Extacted ${urls.size} urls")
+    Future.successful(urls)
   }
 
 }
